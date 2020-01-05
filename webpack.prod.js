a25c7afd5dcefa3d52393dbf8539a4bea1449583
addy <addisonshaw93@gmail.com>
2020-01-05 11:10:07 -0600

    Updating base and dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
   stats: 'normal',
-  optimization: {
-    minimize: true
-  },
-  plugins: [new CompressionPlugin()]
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path].br[query]',
+      algorithm: 'brotliCompress',
+      test: /\.(js|jsx|css|html|svg|jpg)$/,
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 1,
+      deleteOriginalAssets: false
+    }),
+    new MinifyPlugin()
+  ]
 });
+
